b49e367dea8f40292711f8da5e149947690fb98e
takatatsu0905 <takatatsu0905@gmail.com>
2023-05-27 17:02:20 +0900

    メール送信機能作成(5/27)

--- 練習/お問い合わせフォーム/index.php
+++ 練習/お問い合わせフォーム/index.php
@@ -13,6 +13,43 @@ if (!empty($_POST['btn_confirm'])) {
     
     $page_flag = 2;
 
+    // 変数とタイムゾーンを初期化
+    $header = null;
+    $auto_reply_subject = null;
+    $auto_reply_text = null;
+    $admin_reply_subject = null;
+    $admin_reply_text = null;
+    date_default_timezone_set('Asia/Tokyo');
+
+    // ヘッダー情報を設定
+    $header = "MIME-Version: 1.0\n";
+    $header .= "From: TAKATATSU事務所 <ideatakatatsu0905@gmail.com>\n";
+    $header .= "Reply-TO: TAKATATSU事務所 <ideatakatatsu0905@gmail.com>\n";
+
+    // 件名を設定
+    $auto_reply_subject = 'お問い合わせありがとうございます。';
+
+    // 本文を設定
+    $auto_reply_text = "この度はお問い合わせ頂き誠にありがとうございます。下記の内容でお問い合わせを受け付けました。\n\n";
+    $auto_reply_text .= "お問い合わせ日時:" . date("Y-m-d H:i") . "\n";
+    $auto_reply_text .= "氏名:" . $_POST['your_name'] . "\n";
+    $auto_reply_text .= "メールアドレス:" . $_POST['email'] . "\n\n";
+    $auto_reply_text .= "TAKATATSU 事務所";
+
+    // メール送信
+    mb_send_mail($_POST['email'], $auto_reply_subject, $auto_reply_text, $header);
+
+    // 運営側へ送るメール
+    $admin_reply_subject = "お問い合わせを受け付けました";
+
+    // 本文を設定
+    $admin_reply_text = "下記の内容でお問い合わせがありました。\n\n";
+    $admin_reply_text .= "お問い合わせ日時:" . date("Y-m-d H:i") . "\n";
+    $admin_reply_text .= "氏名:" . $_POST['your_name'] . "\n";
+    $admin_reply_text .= "メールアドレス:" . $_POST['email'] . "\n\n";
+
+    // 運営側へメール送信
+    mb_send_mail('ideatakatatsu0905@gmail.com', $admin_reply_subject, $admin_reply_text, $header);
 }
 
 ?>
@@ -53,6 +90,7 @@ if (!empty($_POST['btn_confirm'])) {
 
         </form>
 
+    <!-- 完了ページ -->
     <?php elseif($page_flag === 2):?>
 
         <p>送信が完了しました。</p>
