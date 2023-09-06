0008a7c78947e2c913aecf3b7294fef529c9faa6
takatatsu0905 <takatatsu0905@gmail.com>
2023-09-07 05:31:05 +0900

    メモ閲覧機能作成

--- Laravel勉強/simplenote/routes/web.php
+++ Laravel勉強/simplenote/routes/web.php
@@ -20,3 +20,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/create', 'HomeController@create')->name('create');
 Route::post('/store', 'HomeController@store')->name('store');
+Route::get('/edit/{id}', 'HomeController@edit')->name('edit');
