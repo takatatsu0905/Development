4be54b682afb9ea68597a9697e55fce260d236f5
takatatsu0905 <takatatsu0905@gmail.com>
2023-09-08 11:46:01 +0900

    メモ更新機能、タグ機能の実装

--- Laravel勉強/simplenote/resources/views/create.blade.php
+++ Laravel勉強/simplenote/resources/views/create.blade.php
@@ -11,6 +11,10 @@
                 <div class="form-group">
                     <textarea name='content' class="form-control"rows="10"></textarea>
                 </div>
+                <div class="form-group">
+                    <label for="tag">タグ</label>
+                    <input name="tag" type="text" class="form-control" id="tag" placeholder="タグを入力">
+                </div>
                 <button type='submit' class="btn btn-primary btn-lg">保存</button>
             </form>
         </div>
