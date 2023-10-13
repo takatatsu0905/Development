50c3c1a40f642253af70e30a3eae1ccb5695fe08
takatatsu0905 <takatatsu0905@gmail.com>
2023-10-14 07:23:24 +0900

    MVCおさらい及び、seederを使って仮データを作成

---
+++ Laravel勉強/asobiba/resources/views/todo_list/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>テスト</title>
+</head>
+<body>
+    @if($todo_lists->isNotEmpty())
+        <ul>
+            @foreach($todo_lists as $item)
+                <li>
+                    {{$item->name}}
+                </li>
+            @endforeach
+        </ul>
+    @endif
+</body>
+</html>
