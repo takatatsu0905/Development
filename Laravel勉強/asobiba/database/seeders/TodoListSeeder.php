ecc6adefc18676838c183191c2bafefe7f59b97a
takatatsu0905 <takatatsu0905@gmail.com>
2023-10-22 05:36:10 +0900

    tailwindcssの導入

--- Laravel勉強/asobiba/database/seeders/TodoListSeeder.php
+++ Laravel勉強/asobiba/database/seeders/TodoListSeeder.php
@@ -27,11 +27,6 @@ class TodoListSeeder extends Seeder
                     'created_at' => now(),
                     'updated_at' => now(),
                 ],
-                [
-                    'name' => 'テスト3',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
             ]
         );
     }
